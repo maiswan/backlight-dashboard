c9db7914ae17412e20f9155de01b59f8cc87668a
maiswan <173935047+maiswan@users.noreply.github.com>
2025-09-03 12:06:15 +0800

    Centralize state management

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { DashbaordProvider } from './hooks/useCommandContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <App />
+        <DashbaordProvider>
+            <App />
+        </DashbaordProvider>
     </StrictMode>,
 )
