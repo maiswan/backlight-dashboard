7e40d46702aa9ce8b785c85d76f98954d8f546a8
maiswan <173935047+maiswan@users.noreply.github.com>
2025-09-03 12:00:58 +0800

    Remove old code

--- src/config.d.ts
+++
@@ -1,9 +0,0 @@
-import type { Instruction } from "../../instructions/instructionSchema";
-
-export interface Config {
-    led_count: number;
-    led_order: string;
-    gpio_pin: number;
-    fps: number;
-    instructions: Instruction[];
-}
