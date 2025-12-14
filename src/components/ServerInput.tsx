9d7306b2564a7da3e6a043e1fa9b9184f9fe1169
maiswan <173935047+maiswan@users.noreply.github.com>
2025-12-14 10:36:49 +0800

    Collapse layout by extracting server input

--- src/components/ServerInput.tsx
+++ src/components/ServerInput.tsx
@@ -1,20 +1,19 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 import { useDashboardContext } from "../hooks/useCommandContext";
-import Expandable from "./Expandable";
 
 export default function ServerInput() {
 
     const { server, setServer } = useDashboardContext();
-    const initialServer = useRef(server);
 
     const [tempServer, setTempServer] = useState<string | null>(null);
 
     return (
-        <Expandable title="Server" isInitiallyExpanded={initialServer.current === ""}>
-            <form className="flex flex-row gap-1 m-0" action={() => setServer(tempServer ?? "")}>
-                <input value={tempServer ?? server} onChange={(e) => setTempServer(e.target.value)} placeholder="http://192.168.12.02:12021" className="basis-4/5" />
-                <button className="basis-1/5">Connect</button>
-            </form>
-        </Expandable>
+        <form className="flex flex-col md:flex-row gap-y-2 gap-1 md:gap-y-1 mb-4" action={() => setServer(tempServer ?? "")}>
+            <h1 className="text-nowrap mr-4">maiswan/backlight</h1>
+            <div className="flex-1 flex flex-row gap-1">
+                <input value={tempServer ?? server} onChange={(e) => setTempServer(e.target.value)} placeholder="http://192.168.12.02:12021" />
+                <button className="px-4!">Connect</button>
+            </div>
+        </form>
     )
 }
